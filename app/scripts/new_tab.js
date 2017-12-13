d0ffa4214f9468bc104c2334c0ef8cd8c73ab54f
Ingvaras Merkys <ingvaras.merkys@westernunion.com>
2017-12-13 17:41:51 +0200

    worklfow

--- app/scripts/new_tab.js
+++ app/scripts/new_tab.js
@@ -1,1 +1,21 @@
-console.log("Vilkas pilkas");
+import BFX from 'bitfinex-api-node'
+
+let apiKey = '1';
+let apiSecretKey = '2';
+
+const bfxRest = new BFX(apiKey, apiSecretKey, {version: 1}).rest;
+
+console.log(bfxRest);
+// console.log("Vilkas pilkas");
+// // const BFX = require('bitfinex-api-node');
+//
+// // https://api.bitfinex.com/v1/pubticker/symbol
+//
+// const BFX = require('bitfinex-api-node');
+// const bfxRest = new BFX(apiKey, apiSecretKey, {version: 1}).rest;
+// bfxRest.ticker('BTCUSD', (err, res) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(res);
+// });
